f8d78223a46fd43c11cd1e37719f2cab7e76fe13
minhee <minhee@minheeui-MacBookAir.local>
2026-04-03 20:28:18 +0900

    user-profile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    nickname = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
+    birth_year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nickname
